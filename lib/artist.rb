ba95d958c6cf1a5c403a74e10b152e9fa9f3d995
Kat Maldon <katharinamaldon@gmail.com>
2019-03-13 14:25:21 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,42 @@
+class Artist
+  extend Concerns::Findable
+
+  attr_accessor :name
+  attr_reader :songs
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.create(name)
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end
+
+  def add_song(song)
+    song.artist = self unless song.artist == self
+    self.songs << song unless self.songs.include?(song)
+  end
+
+  def genres
+    self.songs.map do |song|
+      song.genre
+    end.uniq
+  end
+
+end
