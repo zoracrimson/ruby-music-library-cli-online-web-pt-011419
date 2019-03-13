646f51de32c843b600ce58592f601352bde968ea
Kat Maldon <katharinamaldon@gmail.com>
2019-03-13 14:35:11 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,35 +1,35 @@
-class Genre
-  extend Concerns::Findable
+# class Genre
+#   extend Concerns::Findable
 
-  attr_accessor :name, :songs
-  @@all = []
+#   attr_accessor :name, :songs
+#   @@all = []
 
-  def initialize(name)
-    @name = name
-    self.songs = []
-  end
+#   def initialize(name)
+#     @name = name
+#     self.songs = []
+#   end
 
-  def self.all
-    @@all
-  end
+#   def self.all
+#     @@all
+#   end
 
-  def self.destroy_all
-    self.all.clear
-  end
+#   def self.destroy_all
+#     self.all.clear
+#   end
 
-  def save
-    self.class.all << self
-  end
+#   def save
+#     self.class.all << self
+#   end
 
-  def self.create(name)
-    genre = Genre.new(name)
-    genre.save
-    genre
-  end
+#   def self.create(name)
+#     genre = Genre.new(name)
+#     genre.save
+#     genre
+#   end
 
-  def artists
-    @songs.collect do |song|
-      song.artist
-    end.uniq
-  end
-end
+#   def artists
+#     @songs.collect do |song|
+#       song.artist
+#     end.uniq
+#   end
+# end
